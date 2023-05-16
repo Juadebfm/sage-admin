afdcc59d61e0fe36541b5921188014c43462884f
Juadeb <juadebgabriel@gmail.com>
2023-05-16 16:41:22 +0100

    fix

--- script.js
+++ script.js
@@ -11,11 +11,6 @@ allSideMenu.forEach((item) => {
   });
 });
 
-//Date Set up
-
-const date = (document.getElementById("date").innerHTML =
-  new Date().getFullYear());
-
 // TOGGLE SIDEBAR
 const menuBar = document.querySelector("#content nav .bx.bx-menu");
 const sidebar = document.getElementById("sidebar");
@@ -68,34 +63,120 @@ switchMode.addEventListener("change", function () {
   }
 });
 
-// Logout
+// render table
 
-function logout() {
-  localStorage.removeItem("adminlogin");
-  window.location.href = "./login.html";
+const toggleButtons = document.querySelectorAll(".toggle_btns button");
+const pageSize = 25; // Number of entries to show per page
+let currentPage = 1; // Current page number
+
+// Function to render the table with the specified page
+function renderTable(page) {
+  const tableBody = document.querySelector("#studentTable tbody");
+
+  tableBody.classList.add("table-bordered");
+
+  // Calculate the start and end indices for the current page
+  const startIndex = (page - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+
+  // Clear existing table rows
+  tableBody.innerHTML = "";
+
+  // Iterate over the data and create table rows for the current page
+  for (let i = startIndex; i < endIndex; i++) {
+    if (i >= data.length) {
+      break; // Exit the loop if end of data is reached
+    }
+
+    const student = data[i];
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${student.id}</td>
+      <td>${student.title}</td>
+      <td>${truncateText(student.body, 50)}</td>
+      <td>${student.userId}</td>
+    `;
+    tableBody.appendChild(row);
+  }
 }
 
-// Dashboard
-function displayDashboard() {
-  const token = JSON.parse(localStorage.getItem("adminlogin")).token;
-  const requestOptions = {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    redirect: "follow",
-  };
-
-  fetch("https://zlglobalalliance.com.ng/api/admin-dashboard", requestOptions)
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(result); // log the result to the console
-      if (result.dashboard) {
-        // your existing code here
-      } else {
-        console.log("Error: No dashboard data found in API response");
-      }
-    })
-    .catch((error) => console.log("error", error));
+// Function to truncate text to a specified length
+function truncateText(text, maxLength) {
+  if (text.length > maxLength) {
+    return text.substring(0, maxLength) + "...";
+  }
+  return text;
 }
-window.onload = displayDashboard;
+
+// Function to handle pagination buttons
+function handlePagination(event) {
+  const targetButton = event.target;
+
+  if (targetButton.tagName === "BUTTON") {
+    // Remove the active class from all buttons
+    toggleButtons.forEach((button) => {
+      button.classList.remove("active");
+    });
+
+    // Add the active class to the clicked button
+    targetButton.classList.add("active");
+
+    // Get the page number from the button's dataset
+    const pageNumber = parseInt(targetButton.dataset.page);
+
+    // Render the table with the specified page
+    renderTable(pageNumber);
+
+    // Update the current page
+    currentPage = pageNumber;
+  }
+}
+
+// Add event listener to the table container for handling pagination
+document
+  .getElementById("tableContainer")
+  .addEventListener("click", handlePagination);
+
+// Function to initialize the table and pagination
+function initializeTable() {
+  // Get the total number of pages
+  const totalPages = Math.ceil(data.length / pageSize);
+
+  // Create pagination buttons
+  const paginationContainer = document.createElement("div");
+  paginationContainer.classList.add("pagination-container");
+
+  for (let i = 1; i <= totalPages; i++) {
+    const button = document.createElement("button");
+    button.textContent = "Page " + i;
+    button.dataset.page = i;
+
+    // Add the active class to the first page button
+    if (i === 1) {
+      button.classList.add("active");
+    }
+
+    paginationContainer.appendChild(button);
+  }
+
+  // Insert the pagination container before the table
+  const tableContainer = document.getElementById("tableContainer");
+  tableContainer.insertBefore(paginationContainer, tableContainer.firstChild);
+
+  // Render the table with the initial page
+  renderTable(currentPage);
+}
+
+// Call the fetchData function to populate the data from the API
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((jsonData) => {
+    // Store the retrieved data
+    data = jsonData;
+
+    // Initialize the table and pagination
+    initializeTable();
+  })
+  .catch((error) => {
+    console.log("Error fetching data:", error);
+  });
